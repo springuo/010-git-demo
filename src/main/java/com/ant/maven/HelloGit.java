1b8e7cb7ae02a9a7c6c72394967e11cfff43265c
bj <bj@126.com>
2018-09-04 14:25:55 +0800

    描述提交内容

---
+++ src/main/java/com/ant/maven/HelloGit.java
@@ -0,0 +1,15 @@
+package com.ant.maven;
+
+/**
+ * ClassName:HelloGit
+ * Package:com.ant.maven
+ * Description:
+ *
+ * @Date 2018/9/4 10:20
+ * @Author guoxin
+ */
+public class HelloGit {
+    public static void main(String[] args) {
+        System.out.println("Hello Git !!");
+    }
+}
